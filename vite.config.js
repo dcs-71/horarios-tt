7bc9118fa0b88b7f88d1709edfee8e6651de1391
Diego <dpcs22@gmail.com>
2025-10-07 12:41:35 -0500

    vite.config fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/dcs-71/horarios-tt/'
+  base: '/horarios-tt/'
 })
